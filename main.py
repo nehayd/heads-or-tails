ba76c1170c6d7abda7eaa3dd2c54c18f1bf074d1
nehayd <89801073+nehayd@users.noreply.github.com>
2021-10-12 11:27:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+choice = random.randint(0,1)
+
+if choice == 1:
+  print("Heads")
+elif choice == 0:
+  print("Tails")
+
+
+
+
